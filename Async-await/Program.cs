11145de50697535fa4056f923688cd93740c14a4
Gabriele-Cavallo <gabry.cava@gmail.com>
2024-12-13 11:45:31 +0100

    first commit

---
+++ Async-await/Program.cs
@@ -0,0 +1,26 @@
+﻿class Program { 
+    public static async Task Main(){
+        string filePath = "largefile.txt";
+        // string content = await ReadLargeFileAsync(filePath);
+        Task<string> fileTask = ReadLargeFileAsync(filePath);
+        Console.WriteLine("After fileTask");
+
+        string content = await fileTask;
+        Console.WriteLine(content);
+    }
+    static async Task<string> ReadLargeFileAsync(string path) { 
+        string content = "";
+        using (var reader = new StreamReader(path))
+        {
+            string? line;
+            while ((line = await reader.ReadLineAsync()) != null)
+            {
+                content += line;
+                Console.WriteLine(line);
+            }
+        }
+
+        return content;
+    }
+
+}
